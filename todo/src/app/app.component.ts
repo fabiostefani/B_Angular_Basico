952c62cb620c36f6a9384da1d74b637e9fbb430d
Fabio de Stefani <fabiostefani@gmail.com>
2019-09-25 07:04:28 -0300

    Criação do projeto inicial

---
+++ todo/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root', //ISSO SE TRANSFORMA EM UMA TAG HTML.
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  //title = 'todo';
+}
